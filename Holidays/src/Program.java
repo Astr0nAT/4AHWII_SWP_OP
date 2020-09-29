ffde7e663d2e5903dda30762a78655f60d49f406
Astr0nAT <astron.autnr@gmail.com>
2020-09-29 17:57:50 +0200

    Created json file with holidays.

--- Holidays/src/Program.java
+++ Holidays/src/Program.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Program {
     public static void main(String[] args) {
         ArrayList<LocalDate> holidays = new ArrayList<LocalDate>();
-        holidays.add(LocalDate.of(2020, Month.JANUARY, 1));
-        holidays.add(LocalDate.of(2020, Month.JANUARY, 6));
-        holidays.add(LocalDate.of(2020, Month.MARCH, 19));
+        int year = 2020;
+        for(int i = 0; i < 11; i++){
+
+        }
 
     }
 }
