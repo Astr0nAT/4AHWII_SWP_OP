f2b339f4685994c189404831bc0bffbf6374a9d7
Astr0nAT <astron.autnr@gmail.com>
2020-10-05 10:31:49 +0200

    Merge PC Laptop

--- Holidays/src/Program.java
+++ Holidays/src/Program.java
@@ -1,3 +1,11 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -12,31 +20,38 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.*;
 
-public class Program {
+public class Program extends Application {
     private static final Scanner reader = new Scanner(System.in);
+    private static int[] holidaysPerWeekday;
+    private static int[] holidaysPerWeekdayVariable;
+    private static int beginningYear;
+    private static int duration;
 
     public static void main(String[] args) {
         LinkedHashMap<LocalDate, String> holidays;
-        int[] holidaysPerWeekday;
-        int duration;
-        int beginningYear;
+        LinkedHashMap<LocalDate, String> holidaysVariable;
 
-        System.out.print("Beginning year (1920-2120): ");
+        System.out.print("Beginning year (1920-2120) (inclusive): ");
         beginningYear = inputInt(1920, 2120);
-        System.out.print("How many years do you want to calculate: ");
-        duration = inputInt(1, 500);
+        System.out.println("How many years do you want to calculate (1-200)?");
+        duration = inputInt(1, 200);
 
         holidays = getAllHolidaysFromFile(duration, beginningYear);
-        holidays = addVariableHolidays(holidays, duration, beginningYear);
         holidaysPerWeekday = countHolidaysForEachWeekday(holidays, beginningYear);
 
-        printHolidays(holidays);
+        holidaysVariable = getAllHolidaysFromFile(duration, beginningYear);
+        holidaysVariable = addVariableHolidays(holidaysVariable, duration, beginningYear);
+        holidaysPerWeekdayVariable = countHolidaysForEachWeekday(holidaysVariable, beginningYear);
+
+        printHolidays(holidaysVariable);
 
         System.out.println();
 
         for (int i = 0; i < 5; i++) {
-            System.out.printf("%9s:   %d%n", DayOfWeek.of(i + 1), holidaysPerWeekday[i]);
+            System.out.printf("%9s:   %d%n", DayOfWeek.of(i + 1), holidaysPerWeekdayVariable[i]);
         }
+
+        Application.launch(args);
     }
 
     private static void printHolidays(LinkedHashMap<LocalDate, String> holidays) {
@@ -45,12 +60,51 @@ public class Program {
         SortedSet<LocalDate> keys = new TreeSet<>(holidays.keySet());
 
         for (LocalDate key : keys) {
-            System.out.printf("%s   %19s   %9s%n", key.toString(), holidays.get(key), key.getDayOfWeek());
             if (key.getYear() > currentYear) {
                 currentYear = key.getYear();
                 System.out.println();
             }
+            System.out.printf("%s   %19s   %9s%n", key.toString(), holidays.get(key), key.getDayOfWeek());
         }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Weekdays");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Amount");
+
+        // Create a BarChart
+        BarChart<String, Number> barChart = new BarChart<String, Number>(xAxis, yAxis);
+
+        // Series 1 - Data of 2014
+        XYChart.Series<String, Number> dataSeries1 = new XYChart.Series<String, Number>();
+        dataSeries1.setName(beginningYear + " - " + (beginningYear + duration));
+
+        dataSeries1.getData().add(new XYChart.Data<String, Number>("Monday", holidaysPerWeekday[0]));
+        dataSeries1.getData().add(new XYChart.Data<String, Number>("Tuesday", holidaysPerWeekday[1]));
+        dataSeries1.getData().add(new XYChart.Data<String, Number>("Wednesday", holidaysPerWeekday[2]));
+        dataSeries1.getData().add(new XYChart.Data<String, Number>("Thursday", holidaysPerWeekday[3]));
+        dataSeries1.getData().add(new XYChart.Data<String, Number>("Friday", holidaysPerWeekday[4]));
+
+        // Add Series to BarChart.
+        barChart.getData().add(dataSeries1);
+
+        barChart.setTitle("Holidays per weekday from " + beginningYear + " over " + duration + " years.");
+
+        VBox vbox = new VBox(barChart);
+
+        primaryStage.setTitle("JavaFX BarChart");
+        Scene scene = new Scene(vbox, 400, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(300);
+        primaryStage.setWidth(400);
+
+        primaryStage.show();
     }
 
     private static LinkedHashMap<LocalDate, String> addVariableHolidays(LinkedHashMap<LocalDate, String> holidays,
